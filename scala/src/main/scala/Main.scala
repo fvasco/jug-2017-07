2123eb98f667db57bdc38777120031c17e1e9517
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2017-07-01 18:56:01 +0200

    added count occourrences logic, thanks to @fvasco

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -1,6 +1,13 @@
+import scala.collection.immutable.ListMap.empty
 import scala.io.Source.fromResource
 
 object Main extends App {
+
+  implicit class ExtendedList[T](t: List[T]) {
+
+    def occurrences: Map[T, Int] =
+      t.foldLeft(empty[T, Int]) { (a, v) => a + a.get(v).fold(v -> 1)(x => v -> (x + 1)) }
+  }
 
   def quickSort(numbers: List[Int]): List[Int] = numbers match {
     case Nil => Nil
@@ -11,5 +18,5 @@ object Main extends App {
 
   val numbers = fromResource("source").getLines.map(_.toInt).toList
 
-  quickSort(numbers).foreach(println)
+  quickSort(numbers).occurrences.foreach(x => println(s"${x._1}=${x._2}"))
 }
--- scala/src/test/scala/MainTest.scala
+++ scala/src/test/scala/MainTest.scala
@@ -1,12 +1,13 @@
+import Main._
 import org.scalatest.{FlatSpec, Matchers}
-import Main.quickSort
+import scala.util.Random.shuffle
 
 
 class MainTest extends FlatSpec with Matchers {
 
   "Given an empty list" should "return an empty list" in {
 
-    quickSort(List[Int]()) shouldBe List[Int]()
+    quickSort(List[Int]()) shouldBe empty
   }
 
   "Given a list with one element" should "return the same list" in {
@@ -16,11 +17,21 @@ class MainTest extends FlatSpec with Matchers {
 
   "Given a list with more than one elements" should "return an ordered list" in {
 
-    quickSort(List(7, 9, 0, 3, 4 , 1, 5, 8, 2, 6)) shouldBe List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    quickSort(List(7, 9, 0, 3, 4, 1, 5, 8, 2, 6)) shouldBe sorted
   }
 
   "Given a list with more than one repeated elements" should "return an ordered list" in {
 
-    quickSort(List(3, 2, 2, 3, 1, 3)) shouldBe List(1, 2, 2, 3, 3, 3)
+    quickSort(List(3, 2, 2, 3, 1, 3)) shouldBe sorted
+  }
+
+  "Given an ordered list with more than one repeated elements" should "count the occurrences preserving the order" in {
+
+    List(1, 2, 2, 3, 3, 3).occurrences.toSeq should contain inOrderOnly(1 -> 1, 2 -> 2, 3 -> 3)
+  }
+
+  "Given a shuffled list with more than one repeated elements" should "sort it and count the occurrences" in {
+
+    quickSort(shuffle(List(1, 2, 2, 3, 3, 3))).occurrences.toSeq should contain inOrderOnly(1 -> 1, 2 -> 2, 3 -> 3)
   }
 }
