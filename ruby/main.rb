8da5d8d996b14ff1c1c368c69105fe6fcac5595b
Gian Carlo Pace <giancarlo.pace@gmail.com>
2017-07-02 03:03:49 +0200

    minor editings

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,15 +1,9 @@
 def quicksort(l)
   return l if (l.size < 2)
-
   p = l.shift
-
   (quicksort(l.select {|n| n < p}) << p << quicksort(l.select {|n| n >= p})).flatten
 end
 
 numbers = File.readlines('../source').map(&:to_i)
-
 ordered = quicksort(numbers)
-
-occurrences = ordered.group_by(&:itself).map {|k, v| "#{k}=#{v.size}"}
-
-puts occurrences
+puts occurrences = ordered.group_by(&:itself).map {|k, v| "#{k}=#{v.size}"}
