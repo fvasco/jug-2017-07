2a5a132fbf4ff817790421eb15161169075a3b14
Francesco Vasco <fsco_v-github@yahoo.it>
2017-07-03 22:06:30 +0200

    Groovy: add CompileStatic

--- groovy/Main.groovy
+++ groovy/Main.groovy
@@ -2,24 +2,24 @@ import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.util.stream.Collectors
 
-def quicksort(list) {
+@groovy.transform.CompileStatic
+<T extends Comparable<T>> List<T> quicksort(List<T> list) {
     if (list.size() < 2) return list
     def pivot = list[0]
     def items = list.groupBy { it <=> pivot }.withDefault { [] }
     quicksort(items[-1]) + items[0] + quicksort(items[1])
 }
 
-def numbers =
-        Files.lines(FileSystems.getDefault().getPath("..", "source"))
-                .map { line -> Integer.valueOf(line) }
-                .collect(Collectors.toList())
+def numbers = Files.lines(FileSystems.getDefault().getPath("..", "source"))
+        .map { line -> Integer.valueOf(line) }
+        .collect(Collectors.toList())
 
 def ordered = quicksort(numbers)
 
-def occurrences = new TreeMap().withDefault { 0 }
-ordered.forEach({ i ->
+def occurrences = new LinkedHashMap().withDefault { 0 }
+ordered.forEach { i ->
     occurrences.put(i, 1 + occurrences[i])
-})
+}
 
 occurrences.forEach { key, value ->
     println(key + "=" + value)
